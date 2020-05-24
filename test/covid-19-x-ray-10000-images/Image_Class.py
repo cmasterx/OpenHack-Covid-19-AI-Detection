8458d899d1fa38106146ef2e1e87121efdbfec35
Ben Costa <bc234cc@gmail.com>
2020-05-24 00:57:36 -0500

    Deleted unessacary upload

--- test/covid-19-x-ray-10000-images/Image_Class.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-class Image_Class:
-    class type(Enum):
-        NORMAL = 0
-        COVID = 1
-    def __init__(self, file_path, type):
-        self.file_path = file_path
-        self.type = type
-
